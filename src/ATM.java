5acfc4a7a1025dcdc6e5f653cadda38970ff7eb3
Aaron Cho <aars.cho10@gmail.com>
2021-08-30 22:17:04 -0700

    First commit with the actual ATM code.

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,22 @@
-
+import java.util.*;
 public class ATM {
-	ggwp
+	
+	private ArrayList <HashMap<String, Integer>> list;
+	
+	public ATM (String ID) {
+		HashMap <String, Integer> bank = new HashMap<String, Integer> ();
+		bank.put(ID, 0);
+		list.add(bank);
+	}
+	
+	public void deposit (String ID, int amount){
+		for (int i = 0; i < list.size(); i++) {
+			HashMap <String, Integer> temp =  list.get(i);
+			if (temp.containsKey(ID)) {
+				temp.replace(ID, temp.get(ID), temp.get(ID)+amount);
+			}
+		}
+		ATM newAccount = new ATM (ID);
+		newAccount.deposit(ID, amount);
+	}
 }
